f6366af3b3f426d61a6b72069e4dad4ed4cad9be
Marcel Suter <marcel.suter@bzz.ch>
2025-03-11 08:24:31 +0100

    initial commit

---
+++ kitten_bots.py
@@ -0,0 +1,59 @@
+""" Provides the KittenBots class. """
+from datetime import datetime
+import random
+
+
+class KittenBots:
+    """
+    A class to represent a group of kitten bots.
+    """
+    def __init__(self):
+        """
+        Initialize the KittenBots class.
+        :param bots: list of bots
+        """
+        self._bots = []
+
+    def register(self, type: str, hostaddr: str, name: str):
+        new_service = {
+            'service_type': type,
+            'ip': hostaddr,
+            'port': str(random.randint(60100, 60999)),
+            'name': name,
+            'heartbeat': datetime.now()
+        }
+        self._bots.append(new_service)
+        return new_service['port']
+    
+    def heartbeat(self, name):
+        """
+        Heartbeat for a service.
+        :param name:
+        :return:
+        """
+        for service in self._bots:
+            if service['name'] == name:
+                service['heartbeat'] = datetime.now()
+                return 'OK'
+        return 'NOT FOUND'
+    
+    def query(self, service_type: str):
+        """
+        Query the kittens for all kittens of a given type.
+        :param service_type:
+        :return:
+        """
+        results = []
+        for index, service in enumerate(self._bots):
+            age = (datetime.now() - service['heartbeat']).total_seconds()
+            if age > 5:
+                self._bots.pop(index)
+            elif service['service_type'] == service_type:
+                results.append(
+                    {
+                        'ip': service['ip'],
+                        'port': service['port'],
+                        'name': service['name']
+                    }
+                )
+        return str(results)
